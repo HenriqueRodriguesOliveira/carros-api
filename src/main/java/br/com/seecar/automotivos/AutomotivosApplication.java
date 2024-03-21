f6e52a571c59725ba86ca9275a9a8779c265e9af
Henrique Rodrigues Oliveira <henrique_krf@outlook.com>
2024-03-21 13:41:48 -0300

    first commit

---
+++ src/main/java/br/com/seecar/automotivos/AutomotivosApplication.java
@@ -0,0 +1,13 @@
+package br.com.seecar.automotivos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomotivosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomotivosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/seecar/automotivos/AutomotivosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.seecar.automotivos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomotivosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
